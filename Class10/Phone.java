c2c889c92b9bba5cffb9a053fe6cc760a43e89d1
rayvnhere <149324548+rayvnhere@users.noreply.github.com>
2023-12-23 23:11:15 -0500

    Add files via upload

---
+++ Class10/Phone.java
@@ -0,0 +1,22 @@
+public class Phone {
+
+    // T A S K //
+
+    String name;
+    String model;
+    String color;
+    double price;
+    double weight;
+
+    void RingTone(){
+        System.out.println("TruTruTru...");
+    }
+
+    void Resolution(){
+        System.out.println("1080p");
+    }
+
+
+
+
+}
