391d97043c2ed236ea3ac140b4ecd00a207505bd
rayvnhere <149324548+rayvnhere@users.noreply.github.com>
2023-12-23 23:06:25 -0500

    Add files via upload

---
+++ Class15/Notes.java
@@ -0,0 +1,39 @@
+/*
+Access Modifiers:
+      Access modifiers in java help us the control who can access what. There are 4 access
+      levels and we use three keywords to control these levels. private, default, protected and public.
+
+          String accountNumber="1234456";
+        //above line is a field
+
+PRIVATE Variables & Method:
+    Is the most restricted one when used with a variable or method
+    those can't be accessed OUTSIDE of THAT class/package, where he was created
+Can only be accessed within the class it was created in.
+
+DEFAULT fields & Methods
+       Also restricted but little flexible then private.
+       if you dont write any keyword then those r becomes default
+Can be accessed in any class AS LONG AS its in the same PACKAGE
+
+PUBLIC Class, Fields & Methods;
+     accessable for any class, any package even FOR THE ENTIRE Project
+
+How to know when to use Private?
+
+Why we use methods?
+to reuse the code again and again.
+
+Asghar said;
+> Always try to create your methods as PUBLIC
+> your fields as PRIVATE
+
+
+Important note:
+    1) 99% of the METHODS are public.
+    2) 99% of the INSTANCE FIELDS are created as private.
+    3) 99% of STATIC FIELDS are public.
+    4) 99% of the time CLASS are also public
+
+//Final keyword can be understood when I know inheritance pillar of OOP's
+ */
