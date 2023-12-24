04ba14bd8d867523c1c5f6976d1c5fab1c14d7c1
rayvnhere <149324548+rayvnhere@users.noreply.github.com>
2023-12-23 23:00:50 -0500

    Add files via upload

---
+++ Class10/Notes.java
@@ -0,0 +1,56 @@
+/*Object Oriented Programming(OOPs):
+    OOPs is just a style of programming. Just a way of
+    writing code that helps us write large amounts
+    of code easily. It helps us manage and change large
+    amounts of code. There are other ways as well
+    as functional programming and procedural programming.
+Pillars of OOPs:
+    There are four main pillars of OOPs.
+0)	Classes and Objects (by Sr. Asghar)
+(they have multiple classes, but I should know each class)
+    1) Inheritance
+    2) Polymorphism
+    3) Abstraction
+    4) Encapsulation
+Classes and Objects:
+Making classes helps others to use our code, to shareable with others, for their object. Like how I use Scanner Class.
+  (Class is the design for the Code) A class is a template/design/map/idea/blueprint
+    from which objects are created.
+
+Class has attributes and behaviors.
+
+
+
+
+
+String is non primitive data type.String.
+ */
+
+//Every class in java can act as datatype, and all of these datatypes are known as
+//non primitive datatypes.
+
+//Class is also a datatype, and so DOG is a datatype.
+
+        /*
+        Dog => Data type
+        dog1=> is a variable that holds the object
+        new Dog();=> Creating the object of Dog class
+         */
+
+/*How we can share our Classes with other?
+ans= can share in the form's of Jars, in the form of Module's, push it to the
+GitHub etc.
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
